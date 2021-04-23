5da9448fe092f1d088c694ae4d4cb8cc110de281
Mehmet Pazar <mehmet.pazar@teamdefinex.com>
2021-04-23 04:42:24 +0300

    Login Page Router

---
+++ src/views/pages/login/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Selam asdasda</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        
+    },
+}
+</script>
+
+<style>
+
+</style>
