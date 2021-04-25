74f7f332319d818a404b7e7d786779e660f26911
Mehmet Pazar <mehmet.pazar@teamdefinex.com>
2021-04-25 06:01:02 +0300

    Login Page Recaptcha

--- src/main.js
+++ src/main.js
@@ -3,14 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import vueToast from 'vue-toast-notification';
+import i18n from './library/plugins/i18n.js';
+import messageplugin from './library/plugins/messageplugin';
+import Toaster from 'v-toaster'
+import 'v-toaster/dist/v-toaster.css'
 
 Vue.config.productionTip = false
+Vue.use(Toaster, {timeout: 5000})
 
 new Vue({
   router,
   store,
   vuetify,
-  vueToast,
+  i18n,
+  messageplugin,
   render: h => h(App)
 }).$mount('#app')
